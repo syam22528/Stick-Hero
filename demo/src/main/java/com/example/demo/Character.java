440f42512c455cfdcf846bc2ca5d677e6a2d3ff7
syam22528 <syam22528@iiitd.ac.in>
2023-11-13 03:14:11 +0530

    character class

---
+++ demo/src/main/java/com/example/demo/Character.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Character {
+
+    private Image character = new Image("stickhero.png");
+    private Image inverse_character = new Image("inverseStickHero.png");
+
+    public Character() {};
+
+    public void StateUp(){
+    }
+
+    public void StateDown(){
+    }
+
+
+
+
+
+}
--- demo/src/main/java/com/example/demo/StickHero.java
+++ demo/src/main/java/com/example/demo/StickHero.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,7 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class StickHero extends Application {
 
     Stage window;
     Scene Home, Game;
